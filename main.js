82749981f3797bb9a028a18a67f7dacffa0896eb
Larry <peggydbc1217@gmail.com>
2023-08-28 21:56:10 +0800

    2-2flightsearch 剩下pagination沒完成 以及RWD

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import "./assets/scss/all.scss";
+
+//2-2FLIGHTSEARCH 卡片點擊  先用BTN ONCLICK 來DEMO
+const flightCard = document.querySelectorAll(".flightCard");
+
+flightCard.forEach((item) => {
+  item.addEventListener("click", function () {
+    const flightCardLower = item.querySelector(".flightCard-lower");
+    flightCardLower.classList.toggle("flightCard-lower-active");
+  });
+});
